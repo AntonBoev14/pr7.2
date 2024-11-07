ab69e5494f712579d0fccd04e6dee558e50964b3
AntonBoev14 <antonboev421@gmail.com>
2024-11-07 10:06:37 +0300

    Add files via upload

---
+++ pr7.2/pr7.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath> 
+
+int main() {
+    setlocale(0, "ru");
+    double val = 0; 
+    int n = 99;     
+
+    do{
+        val = sqrt(n + val); 
+        n -= 3;              
+    } while (n > 0);        
+
+    double y = sin(val); 
+
+    std::cout << "Результат y = " << y << std::endl;
+
+    return 0;
+}
